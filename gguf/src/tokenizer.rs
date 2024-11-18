5aa4a445ec5825f249c52f2b45739548b332d6ec
YdrMaster <ydrml@hotmail.com>
2024-11-18 13:55:45 +0800

    feat: 支持九格 8B 词表
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- gguf/src/tokenizer.rs
+++ gguf/src/tokenizer.rs
@@ -4,7 +4,7 @@ use std::{
     borrow::Cow,
     str::{from_utf8, from_utf8_unchecked},
 };
-use tokeneer::{utok, Bpe, Method, Tokeneer};
+use tokeneer::{utok, Bpe, Lpe, Method, Tokeneer};
 
 pub struct Tokenizer {
     tokenize: Box<dyn Tokenize>,
@@ -15,6 +15,7 @@ impl GGufModel<'_> {
     pub fn tokenizer(&self) -> Tokenizer {
         match self.tokenizer_ggml_model().unwrap() {
             "llama" => Tokenizer::bpe_from_gguf(self),
+            "fm9g8b" => Tokenizer::lpe_from_gguf(self),
             model => panic!("Unsupported tokenizer model: {model}"),
         }
     }
@@ -78,6 +79,41 @@ impl Tokenizer {
         let eos = gguf.tokenizer_ggml_eos_token_id().unwrap();
 
         let bpe = Bpe::new(vocabs, scores, is_byte, unk);
+        let bos_piece = from_utf8(bpe.decode(bos)).unwrap().to_string();
+        let eos_piece = from_utf8(bpe.decode(eos)).unwrap().to_string();
+
+        let mut tokeneer = Tokeneer::new(bpe);
+        tokeneer.extend_special([(bos_piece, vec![bos]), (eos_piece, vec![eos])]);
+        Self {
+            tokenize: Box::new(tokeneer),
+            replace_space: match (space_exist, replace_exist) {
+                (_, true) => Some('▁'),
+                (true, false) => None,
+                (false, false) => panic!("Unknown user-defined space"),
+            },
+        }
+    }
+
+    fn lpe_from_gguf(gguf: &GGufModel) -> Self {
+        let tokens = gguf.tokenizer_ggml_tokens().unwrap();
+
+        let mut space_exist = false;
+        let mut replace_exist = false;
+        let vocabs = tokens.map(|piece| {
+            let piece = piece.unwrap();
+            match piece {
+                " " => space_exist = true,
+                "▁" => replace_exist = true,
+                _ => {}
+            }
+            piece.as_bytes()
+        });
+
+        let unk = gguf.tokenizer_ggml_unknown_token_id().unwrap();
+        let bos = gguf.tokenizer_ggml_bos_token_id().unwrap();
+        let eos = gguf.tokenizer_ggml_eos_token_id().unwrap();
+
+        let bpe = Lpe::new(vocabs, unk);
         let bos_piece = from_utf8(bpe.decode(bos)).unwrap().to_string();
         let eos_piece = from_utf8(bpe.decode(eos)).unwrap().to_string();
 
