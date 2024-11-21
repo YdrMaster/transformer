ea57ff6d48713563d1a9d7640b7ad99dea9feaad
YdrMaster <ydrml@hotmail.com>
2024-11-21 19:34:13 +0800

    perf: 使用 rayon 加速三线性插值，减少预处理时间
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- models/clip/common/src/image.rs
+++ models/clip/common/src/image.rs
@@ -1,8 +1,9 @@
-﻿use gguf::ggml_quants::digit_layout::layout;
+﻿use gguf::ggml_quants::digit_layout::{layout, types as ty};
 use image::ImageReader;
 use itertools::izip;
-use std::{ops::Deref, path::Path};
-use tensor::{rearrange, Blob, Tensor};
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
+use std::{iter::zip, ops::Deref, path::Path, slice::from_raw_parts_mut};
+use tensor::{rearrange, ArrayLayout, Blob, Tensor};
 
 #[repr(transparent)]
 pub struct Image<T>(Tensor<T>);
@@ -12,8 +13,11 @@ pub struct ImageGrid {
     whole: Image<Blob>,
 }
 
-layout!(Urgb u(8)     ; 3);
-layout!(Frgb e(8)m(23); 3);
+layout!(Urgb u(8)     ; 3); // [u8 ; 3]
+layout!(Frgb e(8)m(23); 3); // [f32; 3]
+
+#[allow(non_camel_case_types)]
+type fdim = f64;
 
 impl Image<Vec<u8>> {
     /// 从文件加载
@@ -67,10 +71,7 @@ where
                     .tile(1, &[grid_x, patch_w])
                     .tile(0, &[grid_y, patch_h])
                     .transpose(&[2, 1]);
-                let mut ans = Tensor::new(Urgb, tiled.shape()).map(Blob::new);
-                rearrange(&mut ans, &tiled.map_slice());
-
-                Some(ans)
+                Some(tiled)
             },
             whole: self.bicubic_resize(find_best_resize(
                 [w, h],
@@ -85,18 +86,21 @@ where
     fn bicubic_resize(&self, [w_, h_]: [usize; 2]) -> Image<Blob> {
         assert_eq!(self.0.dt(), Urgb);
 
-        let mut ans_ = Image(Tensor::new(Urgb, &[h_, w_]).map(Blob::new));
-        let ans = ans_.0.get_mut();
+        let mut ans = Image(Tensor::new(Urgb, &[h_, w_]).map(Blob::new));
+        let data = ans.0.get_mut();
+        let ptr = data.as_mut_ptr() as usize;
+        let len = data.len();
 
         let [w, h] = self.shape();
-        let data = self.0.get();
-        let kw = w as f64 / w_ as f64;
-        let kh = h as f64 / h_ as f64;
+        let data = &**self.0.get();
+        let kw = w as f32 / w_ as f32;
+        let kh = h as f32 / h_ as f32;
 
-        for y_ in 0..h_ {
+        (0..h_).into_par_iter().for_each(|y_| {
+            let dst = unsafe { from_raw_parts_mut(ptr as *mut u8, len) };
             for x_ in 0..w_ {
-                let x = kw * x_ as f64;
-                let y = kh * y_ as f64;
+                let x = kw * x_ as f32;
+                let y = kh * y_ as f32;
                 let dx = x - x.floor();
                 let dy = y - y.floor();
                 // 在原图中的坐标
@@ -104,41 +108,60 @@ where
                 let y = y as isize;
                 // 插值循环
                 for rgb in 0..3 {
-                    let mut c = [0.0f64; 4];
+                    let mut c = [0.; 4];
                     for ic in 0..=3 {
-                        fn calc(c: impl Fn(usize) -> f64, d: f64) -> f64 {
-                            let a0 = c(1);
-                            let d0 = c(0) - a0;
-                            let d2 = c(2) - a0;
-                            let d3 = c(3) - a0;
-
-                            let mut ans = a0;
-                            let mut mul = d;
-
-                            ans += (-d0 / 3. + d2 - d3 / 6.) * mul;
-                            mul *= d;
-
-                            ans += (d0 / 2. + d2 / 2.) * mul;
-                            mul *= d;
-
-                            ans + (-d0 / 6. - d2 / 2. + d3 / 6.) * mul
-                        }
-
                         let data = |y: isize, x: isize| {
                             let x = x.clamp(0, w as isize - 1) as usize;
                             let y = y.clamp(0, h as isize - 1) as usize;
-                            data[(y * w + x) * 3 + rgb] as f64
+                            data[(y * w + x) * 3 + rgb] as f32
                         };
                         c[ic as usize] = calc(|i| data(y - 1 + ic, x - 1 + i as isize), dx);
 
                         let cc = calc(|i| c[i], dy);
-                        ans[(y_ * w_ + x_) * 3 + rgb] = cc.round().clamp(0., 255.) as _;
+                        dst[(y_ * w_ + x_) * 3 + rgb] = cc.round().clamp(0., 255.) as _;
                     }
                 }
             }
+        });
+
+        fn calc(c: impl Fn(usize) -> f32, d: f32) -> f32 {
+            let a0 = c(1);
+            let d0 = c(0) - a0;
+            let d2 = c(2) - a0;
+            let d3 = c(3) - a0;
+
+            let mut ans = a0;
+            let mut mul = d;
+
+            ans += (-d0 / 3. + d2 - d3 / 6.) * mul;
+            mul *= d;
+
+            ans += (d0 / 2. + d2 / 2.) * mul;
+            mul *= d;
+
+            ans + (-d0 / 6. - d2 / 2. + d3 / 6.) * mul
         }
 
-        ans_
+        ans
+    }
+
+    /// NHWC rgb Tensor -> NCHW value Tensor
+    #[inline]
+    pub fn to_nchw(&self) -> Tensor<Blob> {
+        let [w, h] = self.shape();
+        let strides = self.0.strides();
+        let layout = ArrayLayout::new(&[1, h, w, 3], &[0, strides[0], strides[1], 1], 0);
+
+        let ty = match self.0.dt() {
+            self::Urgb => ty::U8,
+            self::Frgb => ty::F32,
+            _ => unreachable!(),
+        };
+        let src = Tensor::from_parts(ty, layout, &**self.0.get()).transpose(&[3, 1, 2]);
+
+        let mut ans = Tensor::new(src.dt(), src.shape()).map(Blob::new);
+        rearrange(&mut ans, &src);
+        ans
     }
 }
 
@@ -181,7 +204,7 @@ impl ImageGrid {
 
 /// 舍入除法
 fn div_round(length: usize, patch_size: usize) -> usize {
-    ((length as f64 / patch_size as f64).round() as usize).max(1)
+    ((length as fdim / patch_size as fdim).round() as usize).max(1)
 }
 
 /// 缩放图片尺寸，使图片可按像素分到图块
@@ -194,9 +217,9 @@ fn find_best_resize(
     // 如果允许放大或需要缩小
     if allow_upscale || w * h > scale_resolution.pow(2) {
         // 保持宽高比缩放尺寸，使面积等于 `sr²`
-        let r = (w as f64 / h as f64).sqrt();
-        w = (scale_resolution as f64 * r) as _;
-        h = (scale_resolution as f64 / r) as _;
+        let r = (w as fdim / h as fdim).sqrt();
+        w = (scale_resolution as fdim * r) as _;
+        h = (scale_resolution as fdim / r) as _;
     }
     // 确保宽高整除图块尺寸
     [w, h].map(|l| div_round(l, patch_size) * patch_size)
@@ -206,8 +229,8 @@ fn find_best_resize(
 fn find_best_grid(size: [usize; 2], max_slices: usize, multiple: usize) -> [usize; 2] {
     /// 宽高比的对数
     #[inline(always)]
-    fn ln_ratio([w, h]: [usize; 2]) -> f64 {
-        (w as f64 / h as f64).ln()
+    fn ln_ratio([w, h]: [usize; 2]) -> fdim {
+        (w as fdim / h as fdim).ln()
     }
 
     let target = ln_ratio(size);
@@ -250,17 +273,18 @@ where
     T: Deref<Target = [u8]>,
 {
     assert_eq!(data.dt(), Urgb);
+    assert!(data.is_contiguous());
     let mut ans = Tensor::new(Frgb, data.shape()).map(Blob::new);
 
-    let src = &**data.get();
-    let ([], dst, []) = (unsafe { ans.get_mut().align_to_mut::<f32>() }) else {
+    let ([], src, []) = (unsafe { data.get().align_to::<[u8; 3]>() }) else {
+        unreachable!()
+    };
+    let ([], dst, []) = (unsafe { ans.get_mut().align_to_mut::<[f32; 3]>() }) else {
         unreachable!()
     };
 
-    assert_eq!(dst.len(), src.len());
-    assert_eq!(dst.len() % 3, 0);
-    for i in 0..dst.len() / 3 {
-        for (dst, &src, mean, std) in izip!(&mut dst[i * 3..], &src[i * 3..], mean, std) {
+    for (dst, src) in zip(dst, src) {
+        for (dst, &src, mean, std) in izip!(dst, src, mean, std) {
             *dst = (src as f32 / 255. - mean) / std;
         }
     }
@@ -287,12 +311,16 @@ fn test() {
     println!("slice image {:?}", time.elapsed());
 
     let [x, y] = slices.grid();
-    let [w, h] = slices.whole().shape();
-    println!("whole: {w}x{h}");
-    for j in 0..y {
-        for i in 0..x {
-            let [w, h] = slices.patch(i, j).shape();
-            println!("patch[{i}, {j}] {w}x{h}")
+    let whole = slices.whole();
+    let [w, h] = whole.shape();
+    let t = whole.to_nchw();
+    println!("whole: {w}x{h}: {:?}/{:?}", t.shape(), t.strides());
+    for i in 0..y {
+        for j in 0..x {
+            let patch = slices.patch(j, i);
+            let [w, h] = patch.shape();
+            let t = patch.to_nchw();
+            println!("patch[{i}, {j}] {w}x{h}: {:?}/{:?}", t.shape(), t.strides());
         }
     }
 }
